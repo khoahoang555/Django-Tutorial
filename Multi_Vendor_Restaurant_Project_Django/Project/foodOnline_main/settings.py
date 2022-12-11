caa2a6a966e64451bfa6a924d3928abf7e9f8625
khoahoang555 <khoahoangyahoo@gmail.com>
2022-12-11 09:53:15 +0700

    upload source code (section 8: Token verification & email configuration)

--- Multi_Vendor_Restaurant_Project_Django/Project/foodOnline_main/settings.py
+++ Multi_Vendor_Restaurant_Project_Django/Project/foodOnline_main/settings.py
@@ -142,3 +142,12 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     messages.WARNING: 'warning'
 }
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <khoaphps12540@fpt.edu.vn>'
